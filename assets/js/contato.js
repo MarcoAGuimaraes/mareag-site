122ac13fbc1f6bf28b725fa72451f1d643fc4bab
Marco Guimaraes <magu1908a@gmail.com>
2026-06-17 08:29:58 -0300

    Release v2 - Home simplificada e contato separado

---
+++ assets/js/contato.js
@@ -0,0 +1,10 @@
+document
+.getElementById('formContato')
+.addEventListener('submit', function(e){
+
+    e.preventDefault();
+
+    alert('Mensagem enviada com sucesso.');
+    alert('Formulário em desenvolvimento.');
+});
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,99 @@
-const hora = new Date().getHours();
-const hero = document.querySelector('.hero');
+/* =========================================================
+MENU MOBILE
+========================================================= */
 
-if(hora >= 6 && hora < 18){
+const menuToggle = document.getElementById('menuToggle');
 
-    hero.style.background = `
-    linear-gradient(
-        rgba(8,18,32,0.40),
-        rgba(8,18,32,0.45)
-    ),
-    url('assets/img/dia.webp')
-    `;
+const mobileMenu = document.getElementById('mobileMenu');
 
-}else{
+menuToggle.addEventListener('click', () => {
 
-    hero.style.background = `
-    linear-gradient(
-        rgba(8,18,32,0.70),
-        rgba(8,18,32,0.75)
-    ),
-    url('assets/img/noite.webp')
-    `;
+    mobileMenu.classList.toggle('active');
+
+});
+
+/* =========================================================
+FECHAR MENU AO CLICAR
+========================================================= */
+
+const mobileLinks = document.querySelectorAll('.mobile-menu a');
+
+mobileLinks.forEach(link => {
+
+    link.addEventListener('click', () => {
+
+        mobileMenu.classList.remove('active');
+
+    });
+
+});
+
+/* =========================================================
+SCROLL REVEAL
+Animação suave ao rolar
+========================================================= */
+
+const reveals = document.querySelectorAll('.reveal');
+
+function revealOnScroll(){
+
+    reveals.forEach(element => {
+
+        const windowHeight = window.innerHeight;
+
+        const revealTop = element.getBoundingClientRect().top;
+
+        const revealPoint = 100;
+
+        if(revealTop < windowHeight - revealPoint){
+
+            element.classList.add('active');
+
+        }
+
+    });
 
 }
-hero.style.backgroundSize = "cover";
-hero.style.backgroundPosition = "center";
-hero.style.backgroundRepeat = "no-repeat";
+
+window.addEventListener('scroll', revealOnScroll);
+
+/* =========================================================
+HEADER SCROLL
+Escurece menu ao rolar
+========================================================= */
+
+const header = document.querySelector('.header');
+
+window.addEventListener('scroll', () => {
+
+    if(window.scrollY > 50){
+
+        /*header.style.background = 'rgba(0,0,0,0.65)';*/
+        header.style.background =
+        header.style.background =
+            'linear-gradient(90deg, rgba(3,18,22,.98), rgba(1,8,10,.98))';
+
+    }else{
+
+        /*header.style.background = 'rgba(0,0,0,0.15)';*/
+        header.style.background =
+            'linear-gradient(90deg, rgba(3,18,22,.98), rgba(1,8,10,.98))';
+    }
+
+});
+
+/* =========================================================
+OBSERVAÇÕES IMPORTANTES
+
+1 - UTILIZAR WEBP OU AVIF
+2 - EVITAR PNG GIGANTE
+3 - COMPRIMIR IMAGENS
+4 - USAR LAZY LOADING
+5 - MANTER VÍDEOS LEVES
+6 - EVITAR MUITAS ANIMAÇÕES
+7 - SITE PREPARADO PARA:
+   - painel admin
+   - carrinho
+   - backend
+   - catálogo dinâmico
+========================================================= */
